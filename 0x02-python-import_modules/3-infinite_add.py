5b4acdf1c3a470edee27c32d62e6014b61ba2362
mwasdex <crowngeemax@gmail.com>
2023-12-08 12:29:41 +0300

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    result = 0
-    if (len(argv) - 1) == 0:
-        print(0)
-    elif (len(argv) - 1) == 1:
-        print(int(argv[1]))
-    elif (len(argv) - 1) == 2:
-        print(int(argv[1]) + int(argv[2]))
-    elif (len(argv) - 1) > 2:
-        a = 1
-        b = a + 1
-        while b < (len(argv) - 1):
-            result = int(argv[a]) + int(argv[b])
-            b = b + 1
-            result = result + int(argv[b])
-
-        print(result)
+    n = 1
+    num = 0
+    while n != len(argv):
+        a = argv[n]
+        num += int(a)
+        n += 1
+    print("{:d}".format(num))
