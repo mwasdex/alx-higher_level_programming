10e3d24b0cd947cb94d8fbbbe21356a43adcfc14
mwasdex <crowngeemax@gmail.com>
2023-12-08 13:14:46 +0300

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    number = 0
+    if roman_string is None or isinstance(roman_string, str) is False:
+        return 0
+    for i in range(len(roman_string)):
+        if roman_string[i] == "M":
+            number += 1000
+            if roman_string[i-1] == "C" and i != 0:
+                number -= 200
+        if roman_string[i] == "D":
+            number += 500
+            if roman_string[i-1] == "C" and i != 0:
+                number -= 200
+        if roman_string[i] == "C":
+            number += 100
+            if roman_string[i-1] == "X" and i != 0:
+                number -= 20
+        if roman_string[i] == "L":
+            number += 50
+            if roman_string[i-1] == "X" and i != 0:
+                number -= 20
+        if roman_string[i] == "X":
+            number += 10
+            if roman_string[i-1] == "I" and i != 0:
+                number -= 2
+        if roman_string[i] == "V":
+            number += 5
+            if roman_string[i-1] == "I" and i != 0:
+                number -= 2
+        if roman_string[i] == "I":
+            number += 1
+    return number
