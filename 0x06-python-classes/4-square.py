3d8171fbc6bd94f87cee0f38aff2e1e5dfb16db2
mwasdex <crowngeemax@gmail.com>
2023-12-21 01:30:54 +0300

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Square:
+    """Write a class Square that defines a square"""
+    def __init__(self, size=0):
+        """Module __init__ for size square validation"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method area return the square Area"""
+        return(self.__size ** 2)
+
+    @property
+    def size(self):
+        """Method property of size and return the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Method size_setter atributte evaluate the value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
