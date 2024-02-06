37f47077c611a533dfec8937730b943701d7a295
mwasdex <crowngeemax@gmail.com>
2024-02-06 03:07:22 +0300

    geometry base II

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    Function class BaseGeometry.
+    """
+    def area(self):
+        """
+        ublic instance method: def area(self): that raises
+        an Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
