7464b4d2a6af33a4da510e4e9dbe4d3f7d0f6921
mwasdex <crowngeemax@gmail.com>
2024-02-06 03:05:53 +0300

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class MyList(list):
+    """
+    class MyList that inherits from list
+    """
+    def print_sorted(self):
+        """
+        Public instance method: that prints the list,
+        but sorted (ascending sort)
+        """
+        print(sorted(self))
